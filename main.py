08e7dc568e13f6c3a73b219b22acfa956efe9b29
pheeria <olzhas.askar@deliveryhero.com>
2021-01-06 22:42:29 +0100

    feat: working woohoo

--- main.py
+++ main.py
@@ -1,24 +1,117 @@
-import requests
-from scorebat import prepare_api_links, prepare_youtube_videos, find_games_per_team, find_teams_per_country
+import logging
+import os
 
-API = 'https://www.scorebat.com'
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    CallbackQueryHandler,
+    CallbackContext,
+    ConversationHandler
+)
+from telegram.ext.filters import Filters
+from telegram.ext.messagehandler import MessageHandler
+from scorebat import (
+    prepare_api_links,
+    prepare_youtube_videos,
+    find_games_per_team,
+    find_teams_per_country
+)
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+COUNTRY, TEAM = range(2)
 COUNTRIES = [
     'SPAIN',
     'GERMANY',
     'ENGLAND'
 ]
 
+ENV = os.environ.get('ENV', 'stg')
+PORT = int(os.environ.get('PORT', '8443'))
+TOKEN = os.environ.get('TOKEN', '')
+
+def start(update: Update, _: CallbackContext) -> int:
+    logger.info(f"started for {update.message.from_user['username']}")
+    keyboard = []
+    for country in COUNTRIES:
+        keyboard.append(InlineKeyboardButton(country, callback_data=country))
+
+    reply_markup = InlineKeyboardMarkup([keyboard])
+    update.message.reply_text('Country?', reply_markup=reply_markup)
+
+    return COUNTRY
+
+
+def country_selected(update: Update, context: CallbackContext) -> int:
+    logger.info(f"country selected: {update.callback_query.data}")
+
+    query = update.callback_query
+    query.answer()
+
+    games, teams = find_teams_per_country(query.data)
+    context.user_data['games'] = games
+    
+    keyboard = []
+    
+    for team in teams:
+        keyboard.append(InlineKeyboardButton(team, callback_data=team))
+    reply_markup = InlineKeyboardMarkup.from_column(keyboard)
+    query.edit_message_text('Team?', reply_markup=reply_markup)
+
+    return TEAM
+
+def team_selected(update: Update, context: CallbackContext) -> int:
+    logger.info(f"team selected: {update.callback_query.data}")
+
+    query = update.callback_query
+    query.answer()
+
+    games = find_games_per_team(context.user_data['games'], query.data)
+    apis = prepare_api_links(games)
+    videos = prepare_youtube_videos(apis)
+    
+    for video in videos:
+        query.edit_message_text(video)
+
+    return ConversationHandler.END
+
+def help_command(update: Update, _: CallbackContext) -> None:
+    update.message.reply_text('Use /start to test this bot.')
+
 def main():
-    response = requests.get(API + '/video-api/v1').json()
-    barcelona = find_games_per_team(response, 'Barcelona')
-    print(find_teams_per_country(response, COUNTRIES[2]))
+    updater = Updater(TOKEN)
 
-    links = prepare_api_links(barcelona)
-    videos = prepare_youtube_videos(links)
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            COUNTRY: [
+                CallbackQueryHandler(country_selected)
+            ],
+            TEAM: [
+                CallbackQueryHandler(team_selected)
+            ],
+        },
+        fallbacks=[
+            MessageHandler(Filters.text, help_command)
+        ],
+    )
 
-    print(videos)
+    updater.dispatcher.add_handler(conv_handler)
 
-        
+    if ENV == 'stg':
+        updater.start_polling()
+    else:
+        updater.start_webhook(listen="0.0.0.0",
+              port=PORT,
+              url_path=TOKEN)
+        updater.bot.setWebhook(f"https://voetbot.herokuapp.com/{TOKEN}")
+
+    updater.idle()
+
 
 if __name__ == '__main__':
     main()
